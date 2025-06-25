e0c51179c78569586a914aa5cc9d3fa7201ac42a
igorkll <frerther@gmail.com>
2025-06-26 00:39:07 +0300

    added wpf app example

---
+++ examples/ApplicationOnWpf/ExampleApplication/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+            this.Topmost = true;
+        }
+
+        private void Reboot_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown", "/r /t 0");
+        }
+
+        private void Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+    }
+}
